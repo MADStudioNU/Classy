080c5640e5611910a94b4e3fb7959f1cb052aa0c
Jordan <skorlir@gmail.com>
2015-09-30 10:50:20 -0500

    Fix: preserve Core modules when adding Class modules

--- src/classy/module/index.js
+++ src/classy/module/index.js
@@ -1,3 +1,12 @@
+
+var extendMethods = function (to, from) {
+  for (var method in from) {
+    if ((from[method] instanceof Function) && to[method] == undefined) {
+      to[method] = from[method];
+    }
+  }
+}
+
 var Classy$ModuleForType = function (type) {
   return function AddModule (moduleFn) {
     return function AddModule$Inner (Classy) {
@@ -6,12 +15,13 @@ var Classy$ModuleForType = function (type) {
       if (type == 'core') moduleFn(moduleClassy);
 
       else if (type == 'class') {
-        // FIXME(jordan): you lose all existing core modules here....
         moduleClassy = function Classy$WithModules (nArgs, constructor) {
           var ClassyImpl$Class = Classy(nArgs, constructor);
           moduleFn(ClassyImpl$Class);
           return ClassyImpl$Class;
         }
+        // NOTE(jordan): preserves core modules
+        extendMethods(moduleClassy, Classy);
       }
 
       return moduleClassy;
