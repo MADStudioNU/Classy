92ef1fb1c2e1f8892cd3aab72a56701092c9435d
Jordan <skorlir@gmail.com>
2015-09-30 11:17:54 -0500

    Fix Selfish index.js to just export Selfish${Simple, Variadic}

--- src/selfish/index.js
+++ src/selfish/index.js
@@ -1,8 +1,7 @@
-var Selfish$Simple   = require('./simple.js')
+var Selfish$Simple = require('./simple.js')
   , Selfish$Variadic = require('./variadic.js')
 
-module.exports = function Selfish () {
-  if (arguments.length > 1)
-    return Selfish$Variadic.apply(null, arguments)
-  return Selfish$Simple(arguments[0])
+module.exports = {
+  simple: Selfish$Simple,
+  variadic: Selfish$Variadic
 }
