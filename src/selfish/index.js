eab824217be7ec8369f03249ef7431c0690aff93
Jordan <skorlir@gmail.com>
2015-10-29 12:36:28 -0500

    Move Selfish into a dep, move sources to be easier to access

--- src/selfish/index.js
+++
@@ -1,7 +0,0 @@
-var Selfish$Simple = require('./simple.js')
-  , Selfish$Variadic = require('./variadic.js')
-
-Selfish = module.exports = {
-  simple: Selfish$Simple,
-  variadic: Selfish$Variadic
-}
--- src/selfish/simple.js
+++
@@ -1,8 +0,0 @@
-var Selfish$Simple = function (constructor, base) {
-  return (function (self) {
-    constructor(self);
-    return self;
-  })(base || Object.create(null))
-}
-
-module.exports = Selfish$Simple;
