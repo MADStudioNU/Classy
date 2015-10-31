ad8468d1b808c22aaacd53775e3e2e8a4698d01c
skorlir <skorlir@gmail.com>
2015-10-31 04:15:14 -0500

    Trim eff.js

--- src/eff.js
+++ src/eff.js
@@ -5,35 +5,6 @@
 //   should avoid it at all costs.
 eff = module.exports
 
-// NOTE(jordan): chain recursively
-// EFFICIENCY: 2(n-1) `apply`s, when n>2
-// compose2(compose2(a, b), compose2(c, d))
-// --> apply compose2_1, apply compose2_2
-// --> apply a, apply b, apply c, apply d
-// 4 args, 6 `apply`s
-eff.compose2 = function (a, b) {
-  return function () {
-    a.apply(null, arguments)
-    b.apply(null, arguments)
-  }
-}
-
-// NOTE(jordan): chain as ordered array
-// EFFICIENCY: n `apply`s
-eff.composeArray = function (fns) {
-  return function () {
-    [].forEach.call(fns, function (fn) {
-      fn.apply(null, arguments)
-    })
-  }
-}
-
-// NOTE(jordan): chain as ordered arguments
-// EFFICIENCY: same as composeArray
-eff.compose = function () {
-  return eff.composeArray(arguments)
-}
-
 // NOTE(jordan): chain as ordered array
 // ONLY APPLY certain fns (usecase: modules.compose)
 // EFFICIENCY: 1 `apply`, n fn calls
@@ -50,18 +21,6 @@ eff.composeApplyLast = function (fns) {
         fn(self)
     })
   }
-}
-
-// NOTE(jordan): apply a bunch of fns to args
-// EFFICIENCY: n `apply`s, n+1 fn calls (1 from applyOn)
-eff.applyEach = function (fns, args) {
-  function applyOn (args) {
-    return function apply (fn) {
-      fn.apply(null, args)
-    }
-  }
-
-  fns.forEach(applyOn(args))
 }
 
 // NOTE(jordan): call a bunch of fns oon target
